5b92426945cc2268569f91dd823e759f9d4c613a
Cezary Morawski <dddthethird@gmail.com>
2024-12-03 16:07:06 +0100

    Dodano zmiane do cofniecia

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 import numpy as np
 
 
+def mnozenie(a, b):
+    return a + b
 
-
+9d81d487f98cc545e041430f5cf259bb2d386f8f
+76193e0ae3cef535a6435bf6d8d93052359f0f36
 def powitanie(imie):
-	print(f"Cześć, {imie}! To świetny dzień na naukę")
+    print(f"Cześć, {imie}! To świetny dzień na naukę")
+
 
 def dodaj_liczby(a, b):
-	return a + b
+    return a + b
+
 
 if __name__ == "__main__":
-	imie = input("Podaj swoje imię: ")
-	powitanie(imie)
+    imie = input("Podaj swoje imię: ")
+    powitanie(imie)
 
-	a = int(input("Podaj pierwszą liczbę: "))
-	b = int(input("Podaj drugą liczbę: "))
-	lista = []
-	for i in range(10):
+    a = int(input("Podaj pierwszą liczbę: "))
+    b = int(input("Podaj drugą liczbę: "))
+    lista = []
+    for i in range(10):
+        (())
 
-
-		((((((()))))))
-
-
-	wynik = dodaj_liczby(a, b)
-	print(f"Wynik dodawania {a} i {b} to: {wynik}")
+    wynik = dodaj_liczby(a, b)
+    print(f"Wynik dodawania {a} i {b} to: {wynik}")
+    
+	print("Zmiana do cofnięcia")
